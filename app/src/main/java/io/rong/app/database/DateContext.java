59cd5eb8aa175ccc3d855cc5fe768566aa734995
Bob <ityangb@163.com>
2015-06-02 10:42:44 +0800

    delete greendao

--- app/src/main/java/io/rong/app/database/DateContext.java
+++ app/src/main/java/io/rong/app/database/DateContext.java
@@ -1,52 +1,52 @@
-package io.rong.app.database;
-
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-
-import de.greenrobot.dao.AbstractDaoMaster;
-import de.greenrobot.dao.AbstractDaoSession;
-import io.rong.database.ConversationMaster;
-
-/**
- * Created by Bob on 15/5/21.
- * database
- */
-public class DateContext {
-
-    private static DateContext mDateContext;
-    private Context mContext;
-    private AbstractDaoMaster daoMaster;
-//    private NoteDao noteDao;
-
-    private AbstractDaoSession daoSession;
-    private Cursor cursor;
-
-    private SQLiteDatabase db;
-
-    private DateContext() {
-
-    }
-
-    public static DateContext getInstance() {
-        if (mDateContext == null) {
-            mDateContext = new DateContext();
-        }
-        return mDateContext;
-    }
-
-
-    private DateContext(Context context){
-        mContext = context;
-        ConversationMaster.DevOpenHelper helper = new ConversationMaster.DevOpenHelper(context,"test_dev",null);
-        db = helper.getWritableDatabase();
-
-
-    }
-
-
-    public static void init(Context context){
-        mDateContext = new DateContext(context);
-    }
-
-}
+//package io.rong.app.database;
+//
+//import android.content.Context;
+//import android.database.Cursor;
+//import android.database.sqlite.SQLiteDatabase;
+//
+//import de.greenrobot.dao.AbstractDaoMaster;
+//import de.greenrobot.dao.AbstractDaoSession;
+//import io.rong.database.ConversationMaster;
+//
+///**
+// * Created by Bob on 15/5/21.
+// * database
+// */
+//public class DateContext {
+//
+//    private static DateContext mDateContext;
+//    private Context mContext;
+//    private AbstractDaoMaster daoMaster;
+////    private NoteDao noteDao;
+//
+//    private AbstractDaoSession daoSession;
+//    private Cursor cursor;
+//
+//    private SQLiteDatabase db;
+//
+//    private DateContext() {
+//
+//    }
+//
+//    public static DateContext getInstance() {
+//        if (mDateContext == null) {
+//            mDateContext = new DateContext();
+//        }
+//        return mDateContext;
+//    }
+//
+//
+//    private DateContext(Context context){
+//        mContext = context;
+//        ConversationMaster.DevOpenHelper helper = new ConversationMaster.DevOpenHelper(context,"test_dev",null);
+//        db = helper.getWritableDatabase();
+//
+//
+//    }
+//
+//
+//    public static void init(Context context){
+//        mDateContext = new DateContext(context);
+//    }
+//
+//}
